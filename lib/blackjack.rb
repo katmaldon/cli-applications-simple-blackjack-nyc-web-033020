d255f4a36f9249700f395c3205fa7d10b94c1c7a
Kat Maldon <katharinamaldon@gmail.com>
2020-03-27 23:51:16 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -64,7 +64,7 @@ end
 def invalid_command
   puts "Please enter a valid command"
   get_user_input
-end	end
+end
 
 
 #####################################################	#####################################################
