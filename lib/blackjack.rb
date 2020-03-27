dcbc8dcf6494643b9526d3bfb5e59e12ba6e0e35
Kat Maldon <katharinamaldon@gmail.com>
2020-03-27 23:34:53 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,38 +1,54 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
-def deal_card
-  # code #deal_card here
+
+def deal_card	
+  r = rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+
+def display_card_total	
+  puts "Your cards add up to #{total}"
 end
 
-def prompt_user
-  # code #prompt_user here
+
+def prompt_user	
+  puts "Type 'h' to hit or 's' to stay"
 end
 
-def get_user_input
-  # code #get_user_input here
+
+def get_user_input	
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+
+def end_game	
+  puts "Sorry, you hit #{total}. Thanks for playing!"
 end
 
-def initial_round
-  # code #initial_round here
+
+def initial_round	
+  total = deal_card + deal_card
+  display_card_total(total)
+  total
 end
+
 
 def hit?
-  # code hit? here
-end
+  # code hit? here	  prompt_user
+  answer = get_user_input
+  if answer == "s"
+    total
+  elsif answer == "h"
+    deal_card + total
+  end
+end	end
 
-def invalid_command
-  # code invalid_command here
-end
+
+def invalid_command	def invalid_command
+  # code invalid_command here	  puts "Command not recognized. Enter: 'Hit' or 'Stay'"
+end	end
 
 #####################################################
 # get every test to pass before coding runner below #
