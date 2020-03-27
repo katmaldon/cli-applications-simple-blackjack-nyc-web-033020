da3dd3f1f6629d57552d2bc44ba65086e6a4c9ca
Kat Maldon <katharinamaldon@gmail.com>
2020-03-27 23:57:02 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,60 +1,74 @@
 def welcome
   puts "Welcome to the Blackjack Table"
-end	end
+end
 
 
-def deal_card	
-  r = rand(1..11)
-end	end
+def deal_card
+  card = rand(1..11)
+  return card
+end
 
 
-def display_card_total	
+def display_card_total(total)
   puts "Your cards add up to #{total}"
-end	end
+end
 
 
-def prompt_user	
+def prompt_user
   puts "Type 'h' to hit or 's' to stay"
-end	end
+end	
 
 
 def get_user_input	def get_user_input
-  # code #get_user_input here	  gets.chomp
+  # code #get_user_input here	  # code #get_user_input here
+  gets.chomp
 end	end
 
 
-def end_game	def end_game(total)
-  # code #end_game here	  puts "Sorry, you hit #{total}. Thanks for playing!"
-end	end
-
-
-def initial_round	def initial_round
-  # code #initial_round here	  total = deal_card + deal_card
-  display_card_total(total)
-  total
-end	end
-
-
-def hit?	def hit?(total)
-  # code hit? here	  prompt_user
-  answer = get_user_input
-  if answer == "s"
-    total
-  elsif answer == "h"
-    deal_card + total
-  end
-end	end
-
-
-def invalid_command	def invalid_command
-  # code invalid_command here	  puts "Command not recognized. Enter: 'Hit' or 'Stay'"
-end	end
-
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
-
-def runner
-  # code runner here
+def end_game(total)
+  puts "Sorry, you hit #{total}. Thanks for playing!"
 end
-    
+
+
+def initial_round
+  card1 = deal_card
+  card2 = deal_card
+  sumcards = card1 + card2
+  display_card_total(sum)
+  return sum
+end
+
+
+def hit?(total)
+  prompt_user
+  input = get_user_input
+  if input == "h"
+    new_card = deal_card
+    total = total + new_card
+  elsif choice == "s"
+    total
+  else
+    invalid_command
+  end
+end
+
+
+def invalid_command
+  puts "Please enter a valid command"
+end
+
+
+#####################################################	#####################################################
+
+
+
+# def runner	
+#   # code runner here	  # code runner here
+#   welcome
+#   card_total = initial_round
+#   until card_total > 21
+#     card_total = hit?(card_total)
+#     display_card_total(card_total)
+#   end
+#   end_game(card_total)
+# end	end
