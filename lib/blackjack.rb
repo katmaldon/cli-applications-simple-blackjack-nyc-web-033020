8e41ca40491de6424119b058516fc4724809001d
Kat Maldon <katharinamaldon@gmail.com>
2020-03-28 00:21:16 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -38,9 +38,10 @@ end
 
 def hit?(card_total)
   prompt_user
-  if get_user_input == 'h'
+  input = get_user_input 
+  if input == 'h'
     card_total += deal_card
-    elsif get_user_input == 's'
+    elsif input == 's'
     card_total
     else invalid_command
   end
